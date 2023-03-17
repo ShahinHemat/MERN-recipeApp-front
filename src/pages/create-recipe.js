4fdcf1443cdc4da9db41f681c04601c5245edf32
Shahin Endregard Hemat <120106335+ShahinHemat@users.noreply.github.com>
2023-03-17 18:28:46 +0100

    Main functionality almost done

--- src/pages/create-recipe.js
+++ src/pages/create-recipe.js
@@ -1,3 +1,82 @@
+import { useState, useRef } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+import { useNavigate } from "react-router-dom";
+
 export const CreateRecipe = () => {
-    return <div>Create Recipe</div>
+    const userID = useGetUserID();
+
+    const [recipe, setRecipe] = useState({
+        name: "",
+        description: '',
+        ingredients: [],
+        instructions: '',
+        imageUrl: '',
+        cookingTime: 0,
+        userOwner: userID,
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setRecipe({ ...recipe, [name]: value });
+    };
+
+    const handleIngredientChange = (e, idx) => {
+        const { value } = e.target;
+        const ingredients = recipe.ingredients;
+        ingredients[idx] = value;
+        setRecipe({ ...recipe, ingredients });
+    };
+
+    const ingredientRef = useRef(null);
+
+    const addIngredient = () => {
+        setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ''] });
+        setTimeout(() => {
+            ingredientRef.current.focus();
+        }, 0);
+    };
+
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:3001/recipes', recipe);
+            alert('Recipe Created');
+            navigate('/');
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <div className="create-recipe">
+            <h2>Create Recipe</h2>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type='text' id='name' name="name" onChange={handleChange} />
+                <label htmlFor="description">Description</label>
+                <textarea id='description' name='description' onChange={handleChange}></textarea>
+
+                <label htmlFor="ingredients">Ingredients</label>
+
+                {recipe.ingredients.map((ingredient, idx) => (
+                    <input key={idx} type='text' name='ingredients' value={ingredient} onChange={(e) => handleIngredientChange(e, idx)} ref={idx === recipe.ingredients.length - 1 ? ingredientRef : null} />
+                ))}
+
+                <button onClick={addIngredient} type='button'>Add Ingredient</button>
+
+                <label htmlFor="instructions">Instructions</label>
+                <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+                <label htmlFor="imageUrl">Image URL</label>
+                <input type='text' id="imageUrl" name="imageUrl" onChange={handleChange} />
+                <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+                <input type='number' id="cookingTime" name="cookingTime" onChange={handleChange} />
+
+                <button type="submit">Create Recipe</button>
+            </form>
+        </div>
+    );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,63 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+
 export const Home = () => {
-    return <div>Home</div>
+    const [recipes, setRecipes] = useState([]);
+    const [savedRecipes, setSavedRecipes] = useState([]);
+
+    const userID = useGetUserID();
+
+    useEffect(() => {
+        const fetchRecipe = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/recipes');
+                setRecipes(response.data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        const fetchSavedRecipe = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3001/recipes/savedRecipes/ids${userID}`);
+                setSavedRecipes(response.data.savedRecipes);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchRecipe();
+        fetchSavedRecipe();
+    }, []);
+
+    const saveRecipe = async (recipeID) => {
+        try {
+            const response = await axios.put('http://localhost:3001/recipes', {recipeID, userID});
+            console.log(response);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Recipes</h1>
+            <ul>
+                {recipes.map((recipe) => (
+                    <li key={recipe._id}>
+                        <div>
+                            <h2>{recipe.name}</h2>
+                            <button onClick={() => saveRecipe(recipe._id)}>Save</button>
+                        </div>
+                        <div className="instructions">
+                            <p>{recipe.instructions}</p>
+                        </div>
+                        <img src={recipe.imageUrl}/>
+                        <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
